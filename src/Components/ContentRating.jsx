b3e1b96bba853f7d2b16cda4b63dcbb619db3e52
JuanKenobi74 <cadillacblack74@gmail.com>
2025-11-06 01:24:09 -0500

    fixed counters

--- src/Components/ContentRating.jsx
+++ src/Components/ContentRating.jsx
@@ -5,37 +5,37 @@ class ContentRating extends Component {
   constructor() {
     super();
     this.state = {
-        likes:0,
-        dislikes:0,
-        handleLike:() => {
-            this.setState((prevState) => ({
-                likes: prevState + 1
-            }));
-        },
-        handleDislike:() => {
-            this.setState((prevState) => ({
-                dislikes: prevState + 1
-            }));
-        }
-    };    
+      likes: 0,
+      dislikes: 0,
+      handleLike: () => {
+        this.setState((prevState) => ({
+          likes: prevState.likes + 1
+        }));
+      },
+      handleDislike: () => {
+        this.setState((prevState) => ({
+          dislikes: prevState.dislikes + 1
+        }));
+      }
+    };
   }
   render() {
     return (
-    <>
-    <h1>Text Content Rating</h1>
-    <div className='content-rating'>
-        <p>Text</p>    
-        <div className='rating-buttons'>
+      <>
+        <h1>Text Content Rating</h1>
+        <div className='content-rating'>
+          <p>Text</p>
+          <div className='rating-buttons'>
 
             <button className='like-button' onClick={this.state.handleLike}>
-                Like ({this.state.likes})
+              Like ({this.state.likes})
             </button>
             <button className="dislike-button" onClick={this.state.handleDislike}>
-                Dislike ({this.state.dislikes})
-            </button>            
+              Dislike ({this.state.dislikes})
+            </button>
+          </div>
         </div>
-    </div>
-    </>
+      </>
     );
   }
 }
